425ac36f16f1faadc0f29787a07bc3a531f0f397
Andres Pontt <none@none.com>
2020-08-30 23:40:22 -0400

    remove unused

--- src/main/java/io/github/andrespontt/gitpusher/controller/GitPusherController.java
+++ src/main/java/io/github/andrespontt/gitpusher/controller/GitPusherController.java
@@ -10,29 +10,17 @@ import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.github.andrespontt.gitpusher.Response;
 
 @RestController
 public class GitPusherController {
-
-    @GetMapping(value = "/api/test/{param}", produces = "application/json")
-    public Response apiTest(@PathVariable final String param) {
-        final Response response = new Response();
-        response.setMessage(param);
-        response.setValue(System.getenv("GITHUB_TOKEN"));
-        return response;
-    }
 
     @GetMapping(value = "/api/git/last-commit")
     public String showLastCommit() throws InvalidRemoteException, TransportException, GitAPIException, IOException {
         final File directory = new File("temp/");
         final String uri = "https://github.com/andrespontt/spring-boot-git-pusher.git";
         Git git = Git.cloneRepository().setURI(uri).setDirectory(directory).call();
-       
-        final RevCommit latestCommit = git.log().setMaxCount(1).call().iterator().next();
+       final RevCommit latestCommit = git.log().setMaxCount(1).call().iterator().next();
         final String latestCommitHash = latestCommit.getName();
         FileUtils.deleteDirectory(directory);
         return latestCommitHash;
